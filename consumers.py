23f1b37c362179a069097e39100eb3b1cecc5b76
fredle09 <le197903@gmail.com>
2024-06-16 04:47:20 +0700

    feat(feature/streaming): use kafka-python for producer instead of pyspark

--- consumers.py
+++ consumers.py
@@ -1,31 +1,13 @@
 # import libs
-from pyspark.sql import SparkSession
-from pyspark.sql import DataFrame
-from pyspark.sql.streaming import StreamingQuery
-from bin.Streaming import Consumer
+from bin.streaming.consumer import Consumer
 
 # import constants
 from _constants import *
 
-import findspark
-findspark.init()
 
 if __name__ == '__main__':
-    spark_session: SparkSession = (
-        SparkSession.builder
-        .master("local[*]")
-        .appName("Kafka-Streaming")
-        .config("spark.jars.packages", ",".join(SPARK_STREAMING_PACKAGES))
-        .config("spark.sql.streaming.schemaInference", "true")
-        .getOrCreate()
-    )
-
     con: Consumer = Consumer(
         topic=CAPTURE_TOPIC,
-        schema_list=CAPTURE_YOUTUBE_SCHEMA_LIST,
-        spark_session=spark_session
     )
 
-    streaming_df: DataFrame = con.get_streaming_df()
-    query: StreamingQuery = streaming_df.writeStream.format("console").start()
-    query.awaitTermination()
+    print(con.get_message())
