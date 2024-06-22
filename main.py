210c903e096e66edf862296fed932beaf931a447
fredle09 <le197903@gmail.com>
2024-06-22 16:54:27 +0700

    testing(feature/store): testing store in mongo atlas

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ import pandas as pd
 from _constants import *
 
 # import bin
-from bin.pipeline import Pipeline
+# from bin.pipeline import Pipeline
+from bin.store import MongoDB, Predict
 
 
 def main():
-    pipe: Pipeline = Pipeline()
-    df: pd.DataFrame = pd.read_csv(
-        "datasets/youtube_dataset_pbvm.csv",
-        header=0
+    # pipe: Pipeline = Pipeline()
+    # df: pd.DataFrame = pd.read_csv(
+    #     "datasets/youtube_dataset_pbvm.csv",
+    #     header=0
+    # )
+    # # print(df[["text"]])
+    # # print(pipe.test(df[["text"]]))
+    # df_preprocessed: pd.DataFrame = pipe.test(df[["text"]])
+    # df_preprocessed.to_csv(
+    #     os.path.join(
+    #         PATH,
+    #         "datasets",
+    #         "youtube_dataset_pbvm_preprocessed.csv"
+    #     ),
+    #     index=False,
+    #     header=True
+    # )
+    # print("Done")
+    MongoDB()
+    pred = Predict(
+        platform="youtube",
+        text="I love this video!",
+        label=0
     )
-    # print(df[["text"]])
-    # print(pipe.test(df[["text"]]))
-    df_preprocessed: pd.DataFrame = pipe.test(df[["text"]])
-    df_preprocessed.to_csv(
-        os.path.join(
-            PATH,
-            "datasets",
-            "youtube_dataset_pbvm_preprocessed.csv"
-        ),
-        index=False,
-        header=True
-    )
-    print("Done")
+
+    pred.save()
+    ...
 
 
 if __name__ == "__main__":
