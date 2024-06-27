b200f92c461e78d08de8b62059557308251e0208
fredle09 <le197903@gmail.com>
2024-06-27 17:05:46 +0700

    feat(feature/visualize): both fetch data and visualize

--- producers.py
+++ producers.py
@@ -8,5 +8,5 @@ from _constants import *
 if __name__ == '__main__':
     prod: Producer = Producer(topic=CAPTURE_TOPIC)
     prod.send_message_from_csv(
-        csv_file_path="youtube_pbvm_Hieu.csv",
+        csv_file_path="reddit_pbvm_Huong.csv",
     )
