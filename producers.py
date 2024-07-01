9bf2fecc5de55f41b6e956e772cca71f6b077f2f
fredle09 <le197903@gmail.com>
2024-07-01 14:28:35 +0700

    feat(feature/producers): make 3 threads, producer data concurrently

--- producers.py
+++ producers.py
@@ -1,12 +1,40 @@
-# import libs
+# import bin
 from bin.streaming.producer import Producer
 
 # import constants
 from _constants import *
 
 
+def main():
+    # Create instances of Producer
+    prod = Producer(topic=CAPTURE_TOPIC)
+
+    # List of tasks to run concurrently
+    threads = [
+        threading.Thread(
+            target=prod.send_message_from_csv,
+            kwargs={
+                "csv_file_path": "reddit_pbvm_Huong.csv"
+            }
+        ),
+        threading.Thread(
+            target=prod.send_message_from_csv,
+            kwargs={
+                "csv_file_path": "tiktok_pbvm_Dat.csv"
+            }
+        ),
+        threading.Thread(
+            target=prod.send_message_from_csv,
+            kwargs={
+                "csv_file_path": "youtube_pbvm_Hieu.csv"
+            }
+        ),
+    ]
+
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
+
 if __name__ == '__main__':
-    prod: Producer = Producer(topic=CAPTURE_TOPIC)
-    prod.send_message_from_csv(
-        csv_file_path="reddit_pbvm_Huong.csv",
-    )
+    # Run the main asyncio loop
+    main()
